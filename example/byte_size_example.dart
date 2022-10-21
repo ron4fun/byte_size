bb482ce45a09d09fc38e9f2c9eff71f6df12e90f
ron4fun <ron2tele@gmail.com>
2022-10-21 14:47:36 +0100

    Update byte_size_example.dart

--- example/byte_size_example.dart
+++ example/byte_size_example.dart
@@ -1,18 +1,20 @@
 import 'package:byte_size/byte_size.dart';
 
 void main() {
-  var size = ByteSize.FromKiloBytes(10000);
-  print(size.toString('MB', 3, 'fr_CA')); // 9,766 MB
+  var size = ByteSize.FromKibiBytes(10000);
+  print(size.toString('MiB', 3, 'fr_CA')); // 9,766 MiB
 
   var json = size.toJson();
   print(json);
-  // {"b":"81920000","B":"10240000.00000000000000000000","KB":"10000.00000000000000000000",
-  // "MB":"9.76562500000000000000","GB":"0.00953674316406250000","TB":"0.00000931322574615479",
-  // "PB":"0.00000000909494701773"}
+  // {b: 81920000, B: 10240000.000000000, KiB: 10000.000000000, 
+  // MiB: 9.765625000, GiB: 0.009536743, TiB: 0.000009313, PiB: 0.000000009, 
+  // KB: 10240.000000000, MB: 10.240000000, GB: 0.010240000, 
+  // TB: 0.000010240, PB: 0.000000010}
 
   var size2 = size.copy();
-  print(size2.toString('KB')); // 10,000 KB
+  print(size2.toString('KiB', 0)); // 10,000 KiB
 
   var size3 = ByteSize.fromJson(json);
-  print(size3.toString('KB')); // 10,000 KB
+  print(size3.toString()); // 9.77 MiB
+  print(size3.toString('', 2, 'en_US', true, false)); // 10.24 MB
 }
